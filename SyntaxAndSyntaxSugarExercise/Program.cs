d5292a3fdf028ee8db277d49a2e1bcc887740d60
Daniel Page <dpage105@gmail.com>
2025-03-25 10:21:39 -0500

    Completed exercise 1

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //initializes two implicitly typed variables
+            var answer = 4;
+            var response =" ";
+            
+            //uses ternary operator to display one of two possible interpolated statements
+            response = answer < 9 ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9";
+            
+            Console.WriteLine(response);
         }
     }
 }
